33768e3cf5153e6072aa17ce2329d99bbc32a349
wujekwaldus <masterwaldi@vp.pl>
2017-01-20 14:41:46 +0100

    StartControllerTest

--- java-quiz-academy/src/test/java/pl/academy/quiz/controller/StartControllerTest.java
+++ java-quiz-academy/src/test/java/pl/academy/quiz/controller/StartControllerTest.java
@@ -1,42 +1,81 @@
 package pl.academy.quiz.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import pl.academy.quiz.model.Question;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = StartControllerTest.StartControllerTestConfiguration.class)
 @WebAppConfiguration
 public class StartControllerTest {
-	
-	 private MockMvc mockMvc;
-	 
-	 @Test
-	 public void shouldGetStartPageWithModelAttributes(){
-//		 StartController st = null;
-//		 mockMvc.perform(get("/"))
-//         .andExpect(status().isOk())
-//         .andExpect(view().name("index"))
-//         .andExpect(forwardedUrl("/WEB-INF/views/index.jsp"));
-////         .andExpect(model().attribute("todos", hasItem(
-////                 allOf(
-////                         hasProperty("id", ),
-////                         hasProperty("description", is("Lorem ipsum")),
-////                         hasProperty("title", is("Foo"))
-////                 )
-////         )));
 
-	 }
-	
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void shouldGetStartPageWithModelAttributes() throws Exception {
+		mockMvc.perform(get("/"))//
+				.andExpect(status().isOk())//
+				.andExpect(view().name("index"))//
+				.andExpect(forwardedUrl("/WEB-INF/views/index.jsp"))//
+				.andExpect(model().attribute("areas", Question.QuestionArea.values()))//
+				.andExpect(model().attribute("levels", Question.QuestionLevel.values()));//
+
+	}
+
 	@Configuration
-	public static class StartControllerTestConfiguration {
-//		@Bean
-//		public StartController startController(){
-//			return new StartController();
-//		}
+	@EnableWebMvc
+	public static class StartControllerTestConfiguration extends WebMvcConfigurerAdapter {
+		@Bean
+		public StartController startController() {
+			return new StartController();
+		}
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		}
+
+		@Bean
+		public ViewResolver viewResolver() {
+			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+			viewResolver.setPrefix("/WEB-INF/views/");
+			viewResolver.setSuffix(".jsp");
+			return viewResolver;
+		}
+
 	}
 }
