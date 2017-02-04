755ef0c43ca9600baeff307ef3aeeeeef2d63767
Waldemar <Waldemar@WujekWaldus>
2017-02-04 10:39:06 +0100

    utf encoding

--- java-quiz-academy/src/main/java/pl/academy/quiz/config/JavaQuizSecurityWebInitializer.java
+++ java-quiz-academy/src/main/java/pl/academy/quiz/config/JavaQuizSecurityWebInitializer.java
@@ -1,7 +1,21 @@
 package pl.academy.quiz.config;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class JavaQuizSecurityWebInitializer extends  AbstractSecurityWebApplicationInitializer {
-
+	  @Override
+	    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+	        super.beforeSpringSecurityFilterChain(servletContext);
+	 
+	        FilterRegistration.Dynamic characterEncodingFilter;
+	        characterEncodingFilter = servletContext.addFilter("encodingFilter",
+	                new CharacterEncodingFilter());
+	        characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+	        characterEncodingFilter.setInitParameter("forceEncoding", "true");
+	        characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
+	    }
 }
